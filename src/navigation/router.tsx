1d54cef47e5ff98e459c6e855f317038c4e5fd57
Ygor Furtado <yfurtado@obsglobal.com>
2024-07-28 10:44:20 -0600

    Use outlet to render elements inside the layout component
    Organized routes

--- src/navigation/router.tsx
+++ src/navigation/router.tsx
@@ -7,12 +7,18 @@ import LandingPage from "../pages/LandingPage/LandingPage";
 import LoginPage from "../pages/LoginPage/LoginPage";
 import NotFoundPage from "../pages/NotFoundPage/NotFoundPage";
 import SignUpPage from "../pages/SignUpPage/SignUpPage";
+import Layout from "../components/Layout/Layout";
 
 const routes = createRoutesFromElements(
   <>
-    <Route path="/register" element={<SignUpPage />} />
-    <Route path="/login" element={<LoginPage />} />
-    <Route path="/" element={<LandingPage />} />
+    <Route path="/" element={<Layout />}>
+      <Route index element={<LandingPage />} />
+    </Route>
+    <Route path="/account">
+      <Route path="register" element={<SignUpPage />} />
+      <Route path="login" element={<LoginPage />} />
+    </Route>
+
     <Route path="*" element={<NotFoundPage />} />
   </>
 );
