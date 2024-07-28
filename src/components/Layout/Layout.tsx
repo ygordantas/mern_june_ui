cbd862bac282f183a4a1029abbf441a6782d776a
Ygor Furtado <yfurtado@obsglobal.com>
2024-07-28 11:18:15 -0600

    Improved navbar

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -20,47 +20,46 @@ export default function Layout({ children }: LayoutProps) {
   return (
     <div data-theme={theme} className={classes.page_container}>
       <Navbar expand="lg" className={classes.navbar}>
-        <Container>
-          <Link to="/">
-            <img
-              src="/src/assets/logo.png"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-              alt="MERN Shop logo"
-            />
-          </Link>
+        <Link to="/">
+          <img
+            src="/src/assets/logo.png"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+            alt="MERN Shop logo"
+          />
+        </Link>
 
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle
+          className={classes.toggle}
+          aria-controls="basic-navbar-nav"
+        />
 
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className={classes.nav_item_container + " me-auto"}>
-              <NavLink to="/shop">
-                <CiShoppingCart />
-                Shop
-              </NavLink>
-              <NavLink to="/posts">
-                <RiAuctionFill />
-                My Posts
-              </NavLink>
-              <NavLink to="/messages">
-                <TbMessages /> Messages
-              </NavLink>
-            </Nav>
-            <Nav className={classes.nav_item_container}>
-              <NavLink to="/settings">
-                <IoSettings /> Settings
-              </NavLink>
-              <Link to="login">
-                <IoLogOut /> Logout
-              </Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className={classes.nav_item_container + " me-auto"}>
+            <NavLink to="/shop">
+              <CiShoppingCart />
+              Shop
+            </NavLink>
+            <NavLink to="/posts">
+              <RiAuctionFill />
+              My Posts
+            </NavLink>
+            <NavLink to="/messages">
+              <TbMessages /> Messages
+            </NavLink>
+          </Nav>
+          <Nav className={classes.nav_item_container}>
+            <NavLink to="/settings">
+              <IoSettings /> Settings
+            </NavLink>
+            <Link to="login">
+              <IoLogOut /> Logout
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
-      <Container fluid>
-        {children}
-      </Container>
+      <Container fluid>{children}</Container>
     </div>
   );
 }
